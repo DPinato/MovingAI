b13661ba8e1136a2413ab3c1caa140667fc5c656
Davide <dvdpnt@hotmail.it>
2016-03-26 20:15:18 +0100

    First commit

---
+++ MovingAI/src/MovingAI.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : MovingAI.cpp
+// Author      : D
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "MovingAI.h"
+
+int main(int argc, char *argv[]) {
+
+	cout << "MovingAI started" << endl;
+
+
+
+
+
+	cout << "MovingAI ended" << endl;
+	return 0;
+}
---
+++ MovingAI/src/MovingAI.h
@@ -0,0 +1,25 @@
+/*
+ * MovingAI.h
+ *
+ *  Created on: 26 Mar 2016
+ *      Author: davide
+ */
+
+#ifndef MOVINGAI_H_
+#define MOVINGAI_H_
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+
+// function declaration
+int main(int argc, char *argv[]);
+
+
+
+#endif /* MOVINGAI_H_ */
